c02b6a0fdcf6324a48de5d85b185406bfa7d7e19
Lonis <164430029+lonis752@users.noreply.github.com>
2025-02-02 13:16:16 -0800

    Minor fixes to homepage

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -15,24 +15,25 @@ export const Content = () => {
                 Korean War Memorial
               </h1>
               <p className="hidden md:block">
-                little write up of the gallery and what to expect inside
+                A solemn tribute to the courage and sacrifice of those who
+                served.
               </p>
             </div>
           </div>
         </Link>
         <Link to="/shroud">
-        <div className="flex flex-row justify-center items-center">
-          <div className="flex flex-col rounded justify-center -mr-14 sm:-mr-32 bg-white sm:p-6 shadow-md z-0 max-w-64 p-2">
-            <h1 className="text-md sm:text-2xl md:text-3xl font-bold">
-              Shroud Work
-            </h1>
-            <p className="hidden md:block">little write up</p>
+          <div className="flex flex-row justify-center items-center">
+            <div className="flex flex-col rounded justify-center -mr-14 sm:-mr-32 bg-white sm:p-6 shadow-md z-0 max-w-64 p-2">
+              <h1 className="text-md sm:text-2xl md:text-3xl font-bold">
+                Shroud Work
+              </h1>
+              <p className="hidden md:block">Themes of memory, loss, and the passage of time.</p>
+            </div>
+            <img
+              className="object-contain rounded-sm w-2/4 shadow-lg shadow-gray-300 hover:transition-shadow hover:duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400"
+              src="https://ik.imagekit.io/lonisk96/Miuh%20Yang/20191026-LMH-4204.jpg"
+            />
           </div>
-          <img
-            className="object-contain rounded-sm w-2/4 shadow-lg shadow-gray-300 hover:transition-shadow hover:duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400"
-            src="https://ik.imagekit.io/lonisk96/Miuh%20Yang/20191026-LMH-4204.jpg"
-          />
-        </div>
         </Link>
 
         <Link to="/textile">
@@ -46,7 +47,7 @@ export const Content = () => {
                 Textile Art
               </h1>
               <p className="hidden md:block">
-                little write up of the gallery and what to expect inside
+                Flowers and the poetics of everyday life.
               </p>
             </div>
           </div>
